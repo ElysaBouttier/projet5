21959f63c736c794ccc217e80e4784d12b514466
Elysa Bouttier <elysa.bouttier@sbsr.fr>
2021-06-25 13:45:45 +0200

    add vendor autoloader

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <label for="avatar">Choose a profile picture:</label>
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
 
-    <input type="file"
-        id="avatar" name="avatar"
-        accept="image/png, image/jpeg">
-</body>
-</html>
+use App\Model\Comment;
+
+new Comment();
+
---
+++ src/model/Comment.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+class Comment
+{
+
+    public function __construct()
+    {
+        echo "je suis un commentaire";
+    }
+}
