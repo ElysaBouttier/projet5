4b51e369fb640a3fdc4faf6e075b01137a88540e
Elysa Bouttier <elysa.bouttier@sbsr.fr>
2021-08-10 16:52:01 +0200

    add message model

---
+++ model/Message.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Model;
+
+class Message
+{
+    public function __contruct()
+    {
+    }
+
+    // Return error message
+    public function setError($message)
+    {
+        log("errror msg");
+        global $alert;
+        $alert = [
+            'alertMessage' => $message
+        ];
+    }
+
+    // Return succes message
+    public function setSuccess($message)
+    {
+        log("Success msg");
+        global $success;
+        $success = [
+            'successMessage' => $message
+        ];
+    }
+}
